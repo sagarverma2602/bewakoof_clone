c7cd7396501151226e49e2cf0db23898d6b8bde6
Sagar Verma <55575131+sagarverma2602@users.noreply.github.com>
2023-07-11 22:32:06 +0530

    Day 1

---
+++ bewakoof/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+// import './components/header/header.html'
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ bewakoof/src/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Header.css'
+import logo from './assets/images/logo_bewakoof.png'
+function Header() {
+  return (
+    <div className='mainheader'>
+        <div className='header1'> 
+            <div className='left-option'>
+                <a href='#'>Offer</a>
+                <a href='#'>Fanbook</a>
+                <a href='#'>Download App</a>
+                <a href='#'>Tribe</a>
+
+            </div>
+            <div className='right-option'>
+                <a href='#'>Contact Us</a>
+                <a href='#'>Track Order</a>
+                
+            </div>
+        </div>
+        <div className='header2'>
+            <div className='left2'>
+                <img src={logo} className='logo'/>
+                <a href='#'> Men</a>
+                <a href='#'> Women</a>
+                <a href='#'> Mobile Covers</a>
+
+
+                
+            </div>
+            <div className='right2'></div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
